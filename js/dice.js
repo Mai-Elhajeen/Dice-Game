a52fbdd2037adbd51c5c1d08fce4ccd2bdbb1c70
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-13 04:38:13 +0300

    update on js code and all functions are done

--- js/dice.js
+++ js/dice.js
@@ -1,103 +1,92 @@
-// ==================== 🎲 Roll Dice ====================
-let imgDice = document.querySelector('.dice');
-let btnNewGame = document.querySelector('.btn--new')
-let btnRollDice = document.querySelector('.btn--roll')
-let btnHold = document.querySelector('.btn--hold')
+// ==================== declare the variables ====================
+const imgDice = document.querySelector('.dice')
+const btnRollDice = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
+const btnNewGame = document.querySelector('.btn--new')
 
-let currentScore0 = document.querySelector('#current--0')
-let currentScore1 = document.querySelector('#current--1')
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
-let player0 = document.querySelector('.player--0');
-let player1 = document.querySelector('.player--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
 
-let totalScore0 = document.querySelector('#score--0')
-let totalScore1 = document.querySelector('#score--1')
+const score0 = document.querySelector('#score--0')
+const score1 = document.querySelector('#score--1')
 
-player0.classList.add('player--active');
-player1.classList.remove('player--active');
+// ==================== start the game ====================
+let playerScores, currentScore, activePlayer, gaming
+const startGame = () => {
+    playerScores = [0, 0]
+    currentScore = 0
+    activePlayer = 0
+    gaming = true
 
-let diceArray = ["./img/dice-1.png", "./img/dice-2.png", "./img/dice-3.png", "./img/dice-4.png", "./img/dice-5.png", "./img/dice-6.png"]
+    imgDice.classList.add('hidden')
 
-let sumCurrent0=0;
-let sumCurrent1=0;
+    current0.innerHTML = 0;
+    current1.innerHTML = 0;
+    score0.innerHTML = 0;
+    score1.innerHTML = 0;
 
-let finalSum0 = 0;
-let finalSum1 = 0;
-btnRollDice.addEventListener('click', () => {
-    randomDice = Math.floor(Math.random()*diceArray.length)
-    imgDice.setAttribute('src', diceArray[randomDice])
+    player0.classList.remove('player--winner')
+    player1.classList.remove('player--winner')
+    player0.classList.add('player--active');
+    player1.classList.remove('player--active');
+}
+startGame();
 
-    let playerActive0 = player0.classList.contains('player--active')
-    if(playerActive0 === true){
-        sumCurrent0 += randomDice+1
-        currentScore0.innerHTML = sumCurrent0
-    }else{
-        sumCurrent1 += randomDice+1
-        currentScore1.innerHTML = sumCurrent1
-    }
-
-    if(diceArray[randomDice] === diceArray[0]){
-        player0.classList.toggle('player--active');
-        player1.classList.toggle('player--active');
-    
-        let playerActive1 = player1.classList.contains('player--active')
-        if(playerActive1 === true){
-            finalSum0 += sumCurrent0
-            totalScore0.innerHTML = finalSum0
-            sumCurrent0 =0
-            currentScore0.innerHTML = sumCurrent0
-        }else{
-            finalSum1 += sumCurrent1
-            totalScore1.innerHTML = finalSum1
-            sumCurrent1 =0
-            currentScore1.innerHTML = sumCurrent1
-        }
-        if(finalSum0 >= 10){
-            player0.classList.add('player--winner')
-            currentScore0 =sumCurrent0 =0
-            currentScore1 =sumCurrent1 =0
-            imgDice.classList.add('hidden');
-
-        }else if(finalSum1 >= 10){
-            player1.classList.add('player--winner')
-            currentScore0 =sumCurrent0 =0
-            currentScore1 =sumCurrent1 =0
-            imgDice.classList.add('hidden');
-        }
-    }
-})
-
-btnHold.addEventListener('click', () => {
+// ==================== switch the game between the gamer ====================
+const switchPlayer = () =>{
+    currentActive = document.getElementById(`current--${activePlayer}`)
+    currentActive.innerHTML = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     player0.classList.toggle('player--active');
     player1.classList.toggle('player--active');
+}
 
-    let playerActive1 = player1.classList.contains('player--active')
-    if(playerActive1 === true){
-        finalSum0 += sumCurrent0
-        totalScore0.innerHTML = finalSum0
-        sumCurrent0 =0
-        currentScore0.innerHTML = sumCurrent0
-    }else{
-        finalSum1 += sumCurrent1
-        totalScore1.innerHTML = finalSum1
-        sumCurrent1 =0
-        currentScore1.innerHTML = sumCurrent1
+// ==================== 🎲 Roll Dice ====================
+const diceArray = ["./img/dice-1.png", "./img/dice-2.png", "./img/dice-3.png", "./img/dice-4.png", "./img/dice-5.png", "./img/dice-6.png"]
+
+btnRollDice.addEventListener('click', () => {
+    imgDice.classList.remove('hidden')
+    imgDice.classList.add('animate')
+    setTimeout(() => {
+        imgDice.classList.remove('animate')
+    }, 1000);
+
+    if(gaming === true){
+        randomDice = Math.floor(Math.random() * diceArray.length)
+        imgDice.setAttribute('src', diceArray[randomDice])
+        currentActive = document.getElementById(`current--${activePlayer}`)
+
+        if(randomDice+1 !== 1){
+            currentScore += randomDice+1
+            currentActive.innerHTML = currentScore
+        }else{
+            switchPlayer();
+        }
     }
+});
 
-    if(finalSum0 >= 10){
-        player0.classList.add('player--winner')
-        currentScore0 =sumCurrent0 =0
-        currentScore1 =sumCurrent1 =0
-        imgDice.classList.add('hidden');
-    }else if(finalSum1 >= 10){
-        player1.classList.add('player--winner')
-        currentScore0 =sumCurrent0 =0
-        currentScore1 =sumCurrent1 =0
-        imgDice.classList.add('hidden');
+// ==================== 📥 Hold Dice ====================
+btnHold.addEventListener('click', () =>{
+    if(gaming === true){
+        scoreActive = document.getElementById(`score--${activePlayer}`)
+        playerScores[activePlayer] += currentScore;
+        scoreActive.innerHTML = playerScores[activePlayer]
+        
+        if(playerScores[activePlayer] >= 10){
+            playerActive = document.querySelector(`.player--${activePlayer}`)
+            playerActive.classList.add('player--winner')
+            currentActive.innerHTML = 0
+
+            gaming =false;
+        }else{
+            switchPlayer();
+        }
     }
 })
 
 // ==================== 🔄 Start New Game ====================
-btnNewGame.addEventListener("click", () => {
-    location.reload();
-});
+btnNewGame.addEventListener('click', startGame)
